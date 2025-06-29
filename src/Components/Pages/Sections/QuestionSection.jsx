668553fdb9b2be10bcc626a57c41e0d0ea36b594
Shehriyar Shamsi <shehriyarshamsi@gmail.com>
2025-06-29 09:18:08 -0700

    question logo

--- src/Components/Pages/Sections/QuestionSection.jsx
+++ src/Components/Pages/Sections/QuestionSection.jsx
@@ -16,7 +16,7 @@ const QuestionSection = () => {
     </div>
       <div className="mx-auto  w-[21.7vw] h-[5.6vw] flex justify-center items-center  rounded-[1.5vw] customGradient-bg2">
         <div className=" w-[21.4vw] h-[5.3vw] rounded-[1.5vw] bg-[#000000]">
-          <img src={SmLogo} alt="" className="sm:px-6 px-3 md:px-10 py-1  md:py-4" />
+          <img src={SmLogo} alt="" className="sm:px-6 px-3 md:px-10 py-1  md:py-4  w-[21.4vw] h-[5.3vw]" />
         </div>
       </div>
       <div className="  w-[73vw] md:w-[51vw] py-[.18vw]  flex justify-center items-center  rounded-[1.9vw] mx-auto bg-[linear-gradient(55deg,#8043ef_0%,#8043ef_0%,#00000075_115%)] ">
