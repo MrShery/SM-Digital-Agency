148efafa619b9833fca2db3929636a180a5e1c85
Shehriyar Shamsi <shehriyarshamsi@gmail.com>
2025-07-01 01:26:00 -0700

    pera size

--- src/Components/SectionTitleInfo.jsx
+++ src/Components/SectionTitleInfo.jsx
@@ -22,14 +22,14 @@ const SectionTitleInfo = ({
       )}
       {title != undefined && (
         <h1
-          className={`sm:bg-[linear-gradient(180deg,#ffff_80%,#000)]  sm:bg-clip-text sm:text-transparent capitalize text-white dark:text-black  text-[9vw] sm:text-[38px] md:text-[5vw] lg:text-[4vw] leading-[9.8vw] sm:leading-[5.5vw] md:leading-[45px] lg:leading-[4vw]  md:px-3 sm:pb-1 ${titleClassName} `}
+          className={`sm:bg-[linear-gradient(180deg,#ffff_80%,#000)]  sm:bg-clip-text sm:text-transparent capitalize text-white dark:text-black  text-[8vw] sm:text-[38px] md:text-[5vw] lg:text-[4vw] leading-[9.8vw] sm:leading-[5.5vw] md:leading-[45px] lg:leading-[4vw]  md:px-3 sm:pb-1 ${titleClassName} `}
         >
           {title}
         </h1>
       )}
       {pera != undefined && (
         <p
-          className={`xs:w-[400px] dark:text-black text-white sm:w-[500px] md-w-[400px] lg:w-[66%] text-[4.9vw] xs:text-[3.9vw] sm:px-0   sm:text-[2.7vw] md-text-[18px] leading-[4.5vw] sm:leading-none lg:text-[1.3vw] py-[1vw] white mx-auto  ${peraClassName}`}
+          className={`xs:w-[400px] dark:text-black text-white sm:w-[500px] md-w-[400px] lg:w-[66%] text-[3.5vw] xs:text-[3.9vw] sm:px-0   sm:text-[2.7vw] md-text-[18px] leading-[4.5vw] sm:leading-none lg:text-[1.3vw] py-[1vw] white mx-auto  ${peraClassName}`}
         >
           {pera}
         </p>
